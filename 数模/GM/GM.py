2ee2a4a5889788184a73fe682153617fbcfd1257
zhu-to-u <2528487925@qq.com>
2023-06-04 19:21:36 +0800

    灰色预测模型

--- 数模/GM/GM.py
+++ 数模/GM/GM.py
@@ -86,52 +86,8 @@ plt.figure(figsize=(12,6),dpi=80)
 plt.plot(df["Pollution"],label="排污总量真实值")
 plt.plot(range(1995,2007),fitted_values+forcast,label="GM预测值",linestyle="--")
 plt.legend()
-Latex(
-    f"拟合结果:$\hat x^{{(1)}}(t)={train.iloc[0]-round(u/a,4)}e^{{{-round(a,4)}(t-1)}}{round(u/a,4)}$"
-)
-test_num=3
-train=df["Pollution"][:-test_num]
-test=df["Pollution"][-test_num:]
-
-#建立GM模型
-from statsmodels.api import add_constant
-from IPython.display import Latex
-
-data=train
-def GM11(data,forcast_len=5,show=False):
-    X_0=data
-    X_1=data.cumsum()
-    Z_1=-X_1.rolling(2).mean().dropna()
-    B=add_constant(Z_1,prepend=False)
-    Y=X_0[1:]
-    U=(np.linalg.inv(B.T @ B)) @ B.T @ Y
-    a=U.iloc[0]
-    u=U.iloc[1]
-    #拟合值
-    fitted_values=[(X_0.iloc[0]-u/a)*(1-np.exp(a))*np.exp(-a*t)for t in range(1,len(data)+1)]
-    #预测值
-    forcast=[(X_0.iloc[0]-u/a)*(1-np.exp(a))*np.exp(-a*t)for t in range(len(data),len(data)+forcast_len)]
-    #相对残差
-    relative_residuals=(abs(data-fitted_values)/data)
-    #级比
-    class_ratio=data[1:]/data[:-1]
-    #级比偏差
-    eta=abs(1-(1-0.5*a)/(1+0.5*a)*(1/class_ratio))
-    if show:
-        print("发展系数:",-a,"\n灰作用量:",u)
-        print("预测值:",forcast)
-        print("拟合值:\n",pd.DataFrame(fitted_values,index=data.index))
-    return fitted_values,forcast,relative_residuals,eta,u,a
-
-fitted_values,forcast,relative_residuals,eta,u,a=GM11(data,show=True)
-
-plt.figure(figsize=(12,6),dpi=80)
-plt.plot(df["Pollution"],label="排污总量真实值")
-plt.plot(range(1995,2007),fitted_values+forcast,label="GM预测值",linestyle="--")
-plt.legend()
-Latex(
-    f"拟合结果:$\hat x^{{(1)}}(t)={train.iloc[0]-round(u/a,4)}e^{{{-round(a,4)}(t-1)}}{round(u/a,4)}$"
-)
-
+equation=f"拟合结果: $\hat x^{{(1)}}(t)={train.iloc[0]-round(u/a,4)}e^{{{-round(a,4)}(t-1)}}{round(u/a,4)}$"
+plt.text(2000,300,equation,fontsize=12)
+#plt.show()
 plt.savefig("/home/zhuheqin/clone/python/数模/GM/拟合结果.png")
 
